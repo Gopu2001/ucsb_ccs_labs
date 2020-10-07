b87715f4f771d6aa7160ea06874cdf5bcade4c2a
Anmol Kapoor <akapoor202@csilvm-01.cs.ucsb.edu>
2020-10-07 00:33:33 -0700

    Adding fill lab00 and 1/3 lab01

---
+++ cs16/lab00/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, world!" << endl << "I am ready to skip CS16 with CMPTGCS 1A" << endl;
+    return 0;
+}
---
+++ cs16/lab01/block.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        cout << "Enter number of rows and columns:" << endl;
+        
+    }
+    return 0;
+}
