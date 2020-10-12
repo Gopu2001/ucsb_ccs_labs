4a6089f0d015c6f7c251cf7e4abbbdc817e37978
Anmol Kapoor <akapoor202@umail.ucsb.edu>
2020-10-11 23:58:55 -0700

    Yay! I passsed all the tests for lab04!

--- cs16/lab04/maxOfArray.cpp
+++ cs16/lab04/maxOfArray.cpp
@@ -2,6 +2,17 @@
 #include <cstdlib>
 #include <iostream>
 
+using namespace std;
+
 int maxOfArray(int a[], int size) {
-  return -42; // STUB!  Replace with correct code.
+  if (size < 1) {
+    cerr << "ERROR: maxOfArray called with size < 1" << endl;
+    exit(1);
+  }
+  int result=a[0];
+  for (int i=1; i<size; i++) {
+    if (a[i] > result)
+      result = a[i];
+  }
+  return result;
 }
