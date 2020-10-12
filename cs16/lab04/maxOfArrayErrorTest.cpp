a422beb0e31565abe865b85e8332a0c99da98dc5
Anmol Kapoor <akapoor202@umail.ucsb.edu>
2020-10-12 00:27:23 -0700

    whoops, forgot to remove the tests from this last onegit status! Eeeekkk!

--- cs16/lab04/maxOfArrayErrorTest.cpp
+++ cs16/lab04/maxOfArrayErrorTest.cpp
@@ -2,6 +2,7 @@
 #include "tddFuncs.h"
 
 int main() {
+  /*
   int empty[] = {};
   // expect this function to result in message to cerr and exit(1);
   assertEquals(0,
@@ -9,6 +10,7 @@ int main() {
                "maxOfArray(empty,0)" );
   // Fill this in with code that calls maxOfArray with a size of zero.
   // so that you can see if the error message prints correctly.
+  */
   return 0;
 
 }
